604a155648bb9922c87ae4d464745c43013068e9
Ove Sundal <osu002@post.uit.no>
2017-07-08 14:16:01 +0200

    feat: Render enemy now works

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    // this.sprite = 'images/enemy-bug.png';
+    //using object.create setups the prototype
+  var obj = Object.create(Enemy.prototype);
+  obj.x = x;
+  obj.y = y;
+  obj.sprite = 'images/enemy-bug.png';
+
+  return obj;
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -50,8 +58,10 @@ Player.prototype.handleInput = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy()];
+var allEnemies = [new Enemy(50, 250)];
 var player = new Player();
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
